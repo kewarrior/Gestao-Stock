ae38eeac2663f11a8b39c193c06c8fbce0d03211
jmartins <joaompinho@ua.pt>
2025-04-10 14:38:22 +0100

    Adicionado Carrinho de Compras no projeto.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,9 +9,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 string enderecoDoServidorWebAPI = "https://localhost:7015";
 
+
+builder.Services.AddScoped<CarrinhoService>();
 builder.Services.AddScoped(sp => new HttpClient
 { 
     BaseAddress = new Uri(enderecoDoServidorWebAPI) 
 });
+
+
 
 await builder.Build().RunAsync();
---
+++ Client/Services/CarrinhoService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Shared.Models;
+
+
+
+namespace Client.Services
+{
+    public class CarrinhoService
+    {
+        private List<Produto> item = new List<Produto>();
+
+        public void AdicionarProduto(Produto produto)
+        {
+            item.Add(produto);
+        }
+
+        public void RemoverProduto(Produto produto)
+        {
+            item.Remove(produto);
+        }
+
+        public List<Produto> ObterProdutos()
+        {
+            return item;
+        }
+
+        public decimal ObterTotal()
+        {
+            return (decimal)item.Sum(p => p.Preco);
+        }
+    }
+}
